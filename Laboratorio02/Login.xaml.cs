05fea9318bcd7e2aeebe819e7320d2e4fba18590
Alumno <Alumno@LAB1504-03>
2024-03-23 12:50:23 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio02/Login.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Laboratorio02
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+
+        string username_in = "admin";
+        string password_in = "password";
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Menu menu = new Menu();
+            menu.ShowDialog();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (username.Text != ""  && password.Password != "")
+            {
+                if(username.Text == username_in && password.Password == password_in)
+                {
+                    MessageBox.Show("Logeo correcto!", "Credenciales validas");
+                    Menu menu = new Menu();
+                    menu.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("Credenciales invalidas, Error");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor ingrese su nombre y contraseña correctas", "Credenciales incorrectas");
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Laboratorio02/Menu.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Laboratorio02
+{
+    /// <summary>
+    /// Lógica de interacción para Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Ingreso_Click(object sender, RoutedEventArgs e)
+        {
+            Ingresos ingresosWindow = new Ingresos();
+
+            ingresosWindow.Show();
+        }
+
+        private void Salida_Click(object sender, RoutedEventArgs e)
+        {
+            // Aquí puedes poner el código para manejar el evento de clic en "Salida"
+            MessageBox.Show("Has seleccionado Salida");
+        }
+
+        private void Conductores_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionado Conductores");
+        }
+
+        private void Transportistas_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionado Transportistas");
+        }
+
+        private void Camiones_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionado Camiones");
+        }
+
+        private void Productos_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionado Productos");
+        }
+
+        private void Cargas_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionado Cargas");
+        }
+
+        private void Ingresos_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionado Ingresos");
+        }
+
+        private void Salidas_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionado Salidas");
+        }
+    }
+}
